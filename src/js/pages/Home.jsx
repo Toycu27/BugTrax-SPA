e2f3cc977b9f608212c32537ea7ee19927e57a9c
Onur Toycu <22518711+Toycu27@users.noreply.github.com>
2022-06-21 10:27:44 +0200

    Centered Design for Home page

--- src/js/pages/Home.jsx
+++ src/js/pages/Home.jsx
@@ -5,46 +5,49 @@ export default function Home() {
     return (
         <div className="container">
             <div className="row mb-3 mt-1">
-                <div className="col-auto"><h1>BugTrax</h1></div>
-            </div>
-
-            <div className="row g-4 mb-4">
-                <p className="fw-normal mb-3 fs-4">
-                    Bug Tracking Software Designed for the Web
-                    <br />
-                    Make it easy for Developers to locate critical information
-                </p>
-                <div className="row">
-                    <div className="col-auto">
-                        <ul className="ms-5">
-                            <li>Bug Form is web specific</li>
-                            <li>Entity Pages with filters</li>
-                            <li>Kanban Board with filters</li>
-                            <li>Navigation searchbar</li>
-                            <li>Project statistics</li>
-                            <li>Comment function</li>
-                        </ul>
-                    </div>
-                    <div className="col-auto">
-                        <ul className="ms-5">
-                            <li>Login and registration</li>
-                            <li>Full responsive design</li>
-                            <li>Auto light and dark mode</li>
-                        </ul>
-                    </div>
-                </div>
+                <div className="col text-center"><h1>BugTrax</h1></div>
             </div>
 
             <div className="row mb-4">
-                <div className="col">
+                <div className="col text-center">
                     <Link data-title="Demo Login" to="/demo/login">
                         <button className="btn btn-primary fs-6 px-5" type="button">Demo Login</button>
                     </Link>
                 </div>
             </div>
 
+            <div className="row g-4 mb-3">
+                <div className="col text-center">
+                    <p className="fw-normal mb-3 fs-4">
+                        Bug Tracking Software Designed for the Web
+                        <br />
+                        Make it easy for Developers to locate critical information
+                    </p>
+                </div>
+            </div>
+
+            <div className="row mb-3 justify-content-center">
+                <div className="col-auto">
+                    <ul className="ms-5">
+                        <li>Bug Form is web specific</li>
+                        <li>Entity Pages with filters</li>
+                        <li>Kanban Board with filters</li>
+                        <li>Navigation searchbar</li>
+                        <li>Project statistics</li>
+                        <li>Comment function</li>
+                    </ul>
+                </div>
+                <div className="col-auto">
+                    <ul className="ms-5">
+                        <li>Login and registration</li>
+                        <li>Full responsive design</li>
+                        <li>Auto light and dark mode</li>
+                    </ul>
+                </div>
+            </div>
+
             <div className="row g-4 mb-4">
-                <div className="col-12 col-lg-10 p-1 bg-secondary">
+                <div className="col p-1 bg-secondary">
                     <div id="carouselCaptions" className="carousel carousel-fade carousel-dark slide" data-bs-ride="carousel">
                         <div className="carousel-inner">
                             <div className="carousel-item active">
